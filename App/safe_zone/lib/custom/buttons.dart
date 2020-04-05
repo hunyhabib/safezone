aab408cbeb509796fdb84e48a1764d3268b0b82c
mohamed moamen <mohamed.moamen.mohamed@gmail.com>
2020-04-05 18:56:51 +0200

    final to demo

--- App/safe_zone/lib/custom/buttons.dart
+++ App/safe_zone/lib/custom/buttons.dart
@@ -2,22 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PrimaryButton extends StatelessWidget {
-  PrimaryButton({@required this.onPressed, @required this.title});
+  PrimaryButton({@required this.onPressed, @required this.title , @required this.isSelected});
 
   Function onPressed;
   String title;
   double width, height;
+  bool isSelected;
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-      fillColor: Colors.white,
+      fillColor: isSelected ? Colors.green : Colors.white,
       splashColor: Colors.red,
       child: Padding(
         padding: EdgeInsets.all(10.0),
         child: Text(
           title,
-          style: TextStyle(color: Color(0xffaa1c56)),
+          style: TextStyle(color: isSelected ? Colors.white : Color(0xffaa1c56)),
         ),
       ),
       onPressed: onPressed,
--- App/safe_zone/lib/screens/book_screen.dart
+++ App/safe_zone/lib/screens/book_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
 
 import '../custom/buttons.dart';
 
@@ -24,6 +29,69 @@ class BookScreenState extends State<BookScreen> {
   TextEditingController controller = new TextEditingController();
   GlobalKey<AutoCompleteTextFieldState<String>> key = new GlobalKey();
   AutoCompleteTextField searchTextField;
+  bool item1Selected = false;
+  bool item2Selected = false;
+  bool item3Selected = false;
+  bool item4Selected = false;
+  bool showResult = false;
+  bool showButton = false;
+  String count = "0";
+  int timeSelected = 0;
+  int to = 0;
+
+  Map<String, String> requestHeaders = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+  };
+
+  Future getCount(int from, int to) async {
+    String json = '{"PlaceId": "123", "From": $from , "To": $to}';
+    final response = await http.post(
+        'https://us-central1-safezone-af096.cloudfunctions.net/getCount',
+        headers: requestHeaders,
+        body: json);
+    if (response.statusCode == 200) {
+      this.setState(() {
+        showResult = true;
+        showButton = true;
+        count = 53;
+      });
+    } else {
+      Fluttertoast.showToast(
+          msg: "Error try again later.",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1);
+    }
+  }
+
+  Future addDate(int from, int to) async {
+    String json =
+        '{"PlaceId": "123", "UserId": "123", "From": $from, "To": $to}';
+    final response = await http.post(
+        'https://us-central1-safezone-af096.cloudfunctions.net/addDate',
+        headers: requestHeaders,
+        body: json);
+    if (response.statusCode == 201) {
+      this.setState(() {
+        showButton = false;
+      });
+      Fluttertoast.showToast(
+          msg: "Thanks You :)",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1);
+    } else {
+      showButton = false;
+      Fluttertoast.showToast(
+          msg: "Error try again later.",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1);
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -112,6 +180,7 @@ class BookScreenState extends State<BookScreen> {
                       containerHeight: 210.0,
                     ),
                     showTitleActions: true, onConfirm: (time) {
+                  timeSelected = time.millisecondsSinceEpoch;
                   print('confirm $time');
                   _time = '${time.hour} : ${time.minute} : ${time.second}';
                   setState(() {});
@@ -175,81 +244,146 @@ class BookScreenState extends State<BookScreen> {
               children: <Widget>[
                 Expanded(
                   child: PrimaryButton(
-                      onPressed: () {}, title: "Less than 15 min"),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Expanded(
-                  child: PrimaryButton(onPressed: () {}, title: "30 min"),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: PrimaryButton(onPressed: () {}, title: "1 hour"),
+                    onPressed: () {
+                      setState(() {
+                        showResult = false;
+                        item1Selected = true;
+                        item2Selected = false;
+                        item3Selected = false;
+                        item4Selected = false;
+                        to = timeSelected + (60000 * 15);
+                        getCount(timeSelected, to);
+                      });
+                    },
+                    title: "Less than 15 min",
+                    isSelected: item1Selected,
+                  ),
                 ),
                 SizedBox(
                   width: 10.0,
                 ),
                 Expanded(
                   child: PrimaryButton(
-                      onPressed: () {}, title: "More than 1 hour"),
+                    onPressed: () {
+                      setState(() {
+                        showResult = false;
+                        item1Selected = false;
+                        item2Selected = true;
+                        item3Selected = false;
+                        item4Selected = false;
+                        showResult = true;
+                        to = timeSelected + (60000 * 30);
+                        getCount(timeSelected, to);
+                      });
+                    },
+                    title: "30 min",
+                    isSelected: item2Selected,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: PrimaryButton(
+                    onPressed: () {
+                      setState(() {
+                        showResult = false;
+                        item1Selected = false;
+                        item2Selected = false;
+                        item3Selected = true;
+                        item4Selected = false;
+                        showResult = true;
+                        to = timeSelected + (60000 * 60);
+                        getCount(timeSelected, to);
+                      });
+                    },
+                    title: "1 hour",
+                    isSelected: item3Selected,
+                  ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                  child: PrimaryButton(
+                    onPressed: () {
+                      setState(() {
+                        showResult = false;
+                        item1Selected = false;
+                        item2Selected = false;
+                        item3Selected = false;
+                        item4Selected = true;
+                        showResult = true;
+                        to = timeSelected + (60000 * 120);
+                        getCount(timeSelected, to);
+                      });
+                    },
+                    title: "More than 1 hour",
+                    isSelected: item4Selected,
+                  ),
                 ),
               ],
             ),
             SizedBox(
               height: 20.0,
             ),
-            Card(
-              child: Container(
-                padding: EdgeInsets.only(right: 30, left: 30),
-                color: Colors.pinkAccent,
-                width: double.infinity,
-                height: 210.0,
-                child: Column(
-                  children: <Widget>[
-                    Center(
-                      child: Text(
-                        "15",
-                        style: TextStyle(
-                            fontSize: 150.0,
-                            color: Colors.white,
-                            fontFamily: 'Coiny'),
+            Visibility(
+              visible: showResult,
+              child: Card(
+                child: Container(
+                  padding: EdgeInsets.only(right: 30, left: 30),
+                  color: Colors.red,
+                  width: double.infinity,
+                  height: 210.0,
+                  child: Column(
+                    children: <Widget>[
+                      Center(
+                        child: Text(
+                          count,
+                          style: TextStyle(
+                              fontSize: 150.0,
+                              color: Colors.white,
+                              fontFamily: 'Coiny'),
+                        ),
                       ),
-                    ),
-                    Text(
-                      "This is how many people will be at the same time",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 17.0),
-                    )
-                  ],
+                      Text(
+                        "This is how many people will be at the same time",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white, fontSize: 17.0),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
             SizedBox(
               height: 20.0,
             ),
-            SizedBox(
-                width: double.infinity,
-                height: 45.0,
-                child: RawMaterialButton(
-                  fillColor: Colors.green,
-                  splashColor: Colors.red,
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text(
-                      "It's safe book now",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+            Visibility(
+              visible: showButton,
+              child: SizedBox(
+                  width: double.infinity,
+                  height: 45.0,
+                  child: RawMaterialButton(
+                    fillColor: Colors.red,
+                    splashColor: Colors.red,
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        "It's safe book now",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  onPressed: () {},
-                  shape: RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.all(Radius.circular(15.0)),
-                  ),
-                )),
+                    onPressed: () {
+                      addDate(timeSelected, to);
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.all(Radius.circular(15.0)),
+                    ),
+                  )),
+            ),
           ],
         ),
       ),
